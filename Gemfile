af2c323c3ec01b6d70656bfaf46eb8d2e145d67d
krimamadhikarmi <madhikrima20@gmail.com>
2024-03-28 17:15:37 +0545

    updated

--- Gemfile
+++ Gemfile
@@ -18,7 +18,8 @@ gem "puma", ">= 5.0"
 gem "jsbundling-rails"
 
 # Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
-gem "turbo-rails", "~> 1.0"
+# gem "turbo-rails", "~> 1.0"
+gem 'turbo-rails', '~> 2.0', '>= 2.0.5'
 
 # Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
 gem "stimulus-rails"
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,50 +1,56 @@
 class TodosController < ApplicationController
-    before_action :set_todo, only: [:show, :edit, :update, :destroy]
+  before_action :set_todo, only: [:show, :edit, :update, :destroy]
 
-    def index
-      @todos = Todo.all
+  def index
+    @todos = Todo.all
+  end
+
+  def show
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render turbo_stream: [
+        turbo_stream.append("todos", partial: "todo", locals: { todo: @todo }),
+        turbo_stream.remove("form"),
+        turbo_stream.prepend("main", partial: "form", locals: { todo: Todo.new })
+      ]
+    else
+      render :new,status: :unprocessable_entity
     end
-  
-    def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @todo.update(todo_params)
+      redirect_to todos_path, notice: "Task was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
-  
-    def new
-      @todo = Todo.new
+  end
+
+  def destroy
+    @todo.destroy
+    respond_to do |format|
+      format.html { redirect_to todos_path, notice: "Task was successfully destroyed." }
+      format.turbo_stream
     end
-  
-    def create
-      @todo = Todo.new(todo_params)
-  
-      if @todo.save
-        redirect_to todos_path, notice: "Task was successfully created."
-      else
-        render :new
-      end
-    end
-  
-    def edit
-    end
-  
-    def update
-      if @todo.update(todo_params)
-        redirect_to todos_path, notice: "Task was successfully updated."
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-      @todo.destroy
-      redirect_to todos_path, notice: "Task was successfully destroyed."
-    end
-  
-    private
-  
-    def set_todo
-      @todo = Todo.find(params[:id])
-    end
-  
-    def todo_params
-      params.require(:todo).permit(:task)
-    end
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:task)
+  end
 end
